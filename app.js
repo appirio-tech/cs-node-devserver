b6582dd72e4cdb6f488b8c14c55856bd533782ae
Jeff Douglas <jeff@jeffdouglas.com>
2012-02-02 06:23:55 -0500

    changed port

--- app.js
+++ app.js
@@ -200,5 +200,6 @@ app.get('/challenge/:id/assets', function(req, res){
 
  });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", port, app.settings.env);
